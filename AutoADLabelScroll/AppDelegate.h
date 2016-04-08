50ce04caa26e01645c8e638e6f956ac84a291e14
rocky <fengjian0213@126.com>
2016-04-08 10:48:27 +0800

    Initial Commit

---
+++ AutoADLabelScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoADLabelScroll
+//
+//  Created by rocky on 16/4/8.
+//  Copyright © 2016年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoADLabelScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoADLabelScroll
+//
+//  Created by rocky on 16/4/8.
+//  Copyright © 2016年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
