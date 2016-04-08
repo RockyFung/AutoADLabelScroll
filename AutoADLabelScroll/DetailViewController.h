336a40c789994102d556d2deb7e8c84dfea4eae8
rocky <fengjian0213@126.com>
2016-04-08 11:04:29 +0800

    creat

---
+++ AutoADLabelScroll/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  AutoADLabelScroll
+//
+//  Created by rocky on 16/4/8.
+//  Copyright © 2016年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+@property (nonatomic, copy) NSString *text;
+@end
